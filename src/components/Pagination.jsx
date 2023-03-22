9e3abccabf4deb17c2d198fe9a89de6049102aee
Pavan Jain <013pavanjain@gmail.com>
2023-03-22 23:16:14 +0530

    Adds Pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Pagination() {
+  let [pageNum, setPageNum] = useState(1);
+  const onPrev = () => {
+    if(pageNum > 1){
+      setPageNum(pageNum-1);
+    }
+  }
+  const onNext = () => {
+    if(pageNum > 0){
+      setPageNum(pageNum+1);
+    }
+  }
   return (
     <div className='flex justify-center m-6'>
-      <div className='border-2 border-r-0 p-1 border-red-500 rounded-l-xl'>Previous</div>
-      <div className='border-2 p-1 border-red-500'>1</div>
-      <div className='border-2 border-l-0  p-1 border-red-500 rounded-r-xl'>Next</div>
+      <button className='border-2 border-r-0 p-1 border-red-500 rounded-l-xl'
+      onClick={onPrev}>Previous</button>
+      <div className='border-2 p-1 border-red-500'>{pageNum}</div>
+      <button className='border-2 border-l-0  p-1 border-red-500 rounded-r-xl'
+      onClick={onNext}>Next</button>
     </div>
   )
 }
