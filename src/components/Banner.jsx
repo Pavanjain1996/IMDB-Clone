7eefaba4d8c2b9c175c506c9a3078fe6e543ad06
Pavan Jain <013pavanjain@gmail.com>
2023-03-23 23:03:10 +0530

    Added API call for trending movies

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Banner() {
+  let [bannerMovie, setBannerMovie] = useState('')
+  
+  useEffect(() => {
+    (function(){
+      axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=13cf82512e78f27808142958d5d4f8f6').then((res) => {
+        setBannerMovie(res.data.results[0]);
+      })
+    })()
+  },[])
   return (
-    <>
-        <div className='bg-banner h-[50vh] md:h-[60vh] bg-center bg-cover flex items-end'>
-          <div className='text-xl md:text-2xl text-white text-center font-bold bg-gray-900 p-4 bg-opacity-50 w-[100%]'>M3GAN</div>
+    <>{
+        bannerMovie === "" ? <h1>Loading</h1> : 
+        <div className={`bg-[url(https://image.tmdb.org/t/p/original/${bannerMovie.backdrop_path})] h-[50vh] md:h-[60vh] bg-center bg-cover flex items-end`}>
+          <div className='text-xl md:text-2xl text-white text-center font-bold bg-gray-900 p-4 bg-opacity-50 w-[100%]'>{bannerMovie.title}</div>
         </div>
+      }
     </>
   )
 }
--- src/components/TrendingMovies.jsx
+++ src/components/TrendingMovies.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function TrendingMovies() {
+  let [trendingMovies, setTrendingMovies] = useState([]);
+  useEffect(() => {
+    (function(){
+      axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=13cf82512e78f27808142958d5d4f8f6').then((res) => {
+        setTrendingMovies(res.data.results);
+      })
+    })()
+  }, [])
+  
   return (
     <div>
       <div className='text-center font-bold text-2xl p-6 text-red-500'>Trending Movies</div>
       <div className='flex flex-wrap justify-center'>
-        <div className='bg-[url(https://www.online-tech-tips.com/wp-content/uploads/2021/07/13-save-image-paint.jpg)]
-        bg-center bg-cover w-[160px] h-[30vh] md:w-[180px] md:h-[40vh] m-4 rounded-xl hover:scale-110 flex items-end'>
-          <div className='text-white text-center font-bold bg-gray-900 p-1 bg-opacity-90 w-[100%] rounded-b-xl'>Hello</div>
+      {
+        trendingMovies.map((movie) => {
+          return <div className={`bg-[url(https://image.tmdb.org/t/p/original/${movie.poster_path})] bg-center bg-cover w-[160px] h-[30vh] md:w-[180px] md:h-[40vh] m-4 rounded-xl hover:scale-110 flex items-end`}>
+                  <div className='text-white text-center font-bold bg-gray-900 p-1 bg-opacity-90 w-[100%] rounded-b-xl'>{movie.title}</div>
         </div>
+        })
+      }
       </div>
     </div>
   )
