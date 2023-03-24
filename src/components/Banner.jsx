c922b1d51543d20cdc46f5639bc5c3a5580c42b5
Pavan Jain <013pavanjain@gmail.com>
2023-03-24 22:00:00 +0530

    Added posters ad banners

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Audio } from 'react-loader-spinner'
 
 function Banner() {
   let [bannerMovie, setBannerMovie] = useState('')
@@ -13,8 +14,10 @@ function Banner() {
   },[])
   return (
     <>{
-        bannerMovie === "" ? <h1>Loading</h1> : 
-        <div className={`bg-[url(https://image.tmdb.org/t/p/original/${bannerMovie.backdrop_path})] h-[50vh] md:h-[60vh] bg-center bg-cover flex items-end`}>
+        bannerMovie === "" ? <div className='flex justify-center'>
+          <Audio height="80" width="80" radius="9" color="gray" secondaryColor="gray" ariaLabel="loading"/>
+        </div> : 
+        <div className='h-[60vh] md:h-[70vh] bg-center bg-cover flex items-end' style={{backgroundImage: `url(https://image.tmdb.org/t/p/original/${bannerMovie.backdrop_path})`}}>
           <div className='text-xl md:text-2xl text-white text-center font-bold bg-gray-900 p-4 bg-opacity-50 w-[100%]'>{bannerMovie.title}</div>
         </div>
       }
--- src/components/TrendingMovies.jsx
+++ src/components/TrendingMovies.jsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Audio } from 'react-loader-spinner';
 
 function TrendingMovies() {
   let [trendingMovies, setTrendingMovies] = useState([]);
   useEffect(() => {
     (function(){
-      axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=13cf82512e78f27808142958d5d4f8f6').then((res) => {
+      axios.get('https://api.themoviedb.org/3/trending/all/day?api_key=13cf82512e78f27808142958d5d4f8f6&page=1').then((res) => {
         setTrendingMovies(res.data.results);
       })
     })()
@@ -15,9 +16,12 @@ function TrendingMovies() {
     <div>
       <div className='text-center font-bold text-2xl p-6 text-red-500'>Trending Movies</div>
       <div className='flex flex-wrap justify-center'>
-      {
+      {trendingMovies === [] ? <div className='flex justify-center'>
+          <Audio height="80" width="80" radius="9" color="gray" secondaryColor="gray" ariaLabel="loading"/>
+        </div> : 
         trendingMovies.map((movie) => {
-          return <div className={`bg-[url(https://image.tmdb.org/t/p/original/${movie.poster_path})] bg-center bg-cover w-[160px] h-[30vh] md:w-[180px] md:h-[40vh] m-4 rounded-xl hover:scale-110 flex items-end`}>
+          return <div key={movie.id} className='bg-center bg-cover w-[160px] h-[30vh] md:w-[180px] md:h-[40vh] m-4 rounded-xl hover:scale-110 flex items-end'
+                  style={{backgroundImage: `url(https://image.tmdb.org/t/p/original/${movie.poster_path})`}}>
                   <div className='text-white text-center font-bold bg-gray-900 p-1 bg-opacity-90 w-[100%] rounded-b-xl'>{movie.title}</div>
         </div>
         })
