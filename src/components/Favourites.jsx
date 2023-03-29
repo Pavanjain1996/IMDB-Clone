237ef5d47eece18545028aaa462f975628ec4b53
Pavan Jain <013pavanjain@gmail.com>
2023-03-29 22:35:04 +0530

    Added fav movies to favourite page

--- src/components/Favourites.jsx
+++ src/components/Favourites.jsx
@@ -1,14 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Pagination from "./Pagination";
 
 function Favourites() {
-  let favs;
-  if (localStorage.getItem("favMovies") === null) {
-    favs = [];
-  } else {
-    favs = JSON.parse(localStorage.getItem("favMovies"));
+  let favMovieIds = [];
+  if (localStorage.getItem("favMovieIds") === null) {
+    favMovieIds = [];
   }
-  let [favMovies, setFavMovies] = useState(favs);
+  else {
+    favMovieIds = JSON.parse(localStorage.getItem("favMovieIds"))
+  }
+  let favMovies = []
+  if (localStorage.getItem("favMovies") === null) {
+    favMovies = [];
+  }
+  else {
+    favMovies = JSON.parse(localStorage.getItem("favMovies"))
+  }
+
+  let [favs, setFavs] = useState(favMovieIds);
+  useEffect(() => {
+    localStorage.setItem('favMovieIds', JSON.stringify(favs));
+  }, [favs])
+
+  let [favoriteMovies, setFavouriteMovies] = useState(favMovies);
+  useEffect(() => {
+    localStorage.setItem('favMovies', JSON.stringify(favoriteMovies));
+  }, [favoriteMovies])
+
+  const removeMovieFromFavs = (id) => {
+    let newset = favs.filter((movie_id) => {return movie_id!==id});
+    setFavs(newset);
+    newset = favoriteMovies.filter((movie) => {return movie.id!==id});
+    setFavouriteMovies(newset);
+  }
+
   return (
     <>
       <div className="m-6 flex justify-center space-x-2">
@@ -31,7 +56,8 @@ function Favourites() {
         <input
           type="number"
           min="1"
-          max="20"
+          max="20000"
+          value={favs.length}
           className="border-2 text-center"
         />
       </div>
@@ -94,8 +120,14 @@ function Favourites() {
           </thead>
           <tbody className="divide-y divide-gray-100 border-t border-gray-100">
             {
-              favMovies.map((movie) => {
-                return <Row id={movie}></Row>
+              favoriteMovies.map((movie) => {
+                return <Row id={movie.id} 
+                            name={movie.title !== undefined ? movie.title : movie.name}
+                            poster={movie.backdrop_path}
+                            ratings={movie.vote_average}
+                            popularity={movie.popularity}
+                            remove={removeMovieFromFavs}>
+                        </Row>
               })
             }
           </tbody>
@@ -113,25 +145,25 @@ function Row(props) {
         <th className="flex gap-3 px-6 py-4 font-normal text-gray-900r items-center">
           <img
             className="h-[7rem] w-[14rem] object-fit"
-            src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            src={`https://image.tmdb.org/t/p/w500/${props.poster}`}
             alt=""
           />
           <span className="absolute right-0 bottom-0 h-2 w-2 rounded-full bg-green-400 ring ring-white"></span>
           <div className="text-sm">
-            <div className="font-medium text-gray-700">{props.id}</div>
+            <div className="font-medium text-gray-700">{props.name}</div>
           </div>
         </th>
-        <td className="px-6 py-4 text-center font-bold">10</td>
-        <td className="px-6 py-4 text-center font-bold">10</td>
+        <td className="px-6 py-4 text-center font-bold">{props.ratings}</td>
+        <td className="px-6 py-4 text-center font-bold">{props.popularity}</td>
         <td className="px-6 py-4 text-center">
           <span className="inline-flex items-center gap-1 rounded-full bg-green-300 px-2 py-1 text-xs font-semibold text-black">
             All Genre
           </span>
         </td>
         <td className="px-6 py-4 text-center">
-          <span className="inline-flex items-center gap-1 rounded-full bg-gray-300 px-2 py-1 text-xs font-semibold text-red-700">
+          <button className="inline-flex items-center gap-1 rounded-full bg-gray-300 px-2 py-1 text-xs font-semibold text-red-700" onClick={() => props.remove(props.id)}>
             Delete
-          </span>
+          </button>
         </td>
       </tr>
     </>
