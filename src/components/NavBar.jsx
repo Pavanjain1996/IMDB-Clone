7b31545e85d529bfcdc73b71bd19e7a750c5a2b0
Pavan Jain <013pavanjain@gmail.com>
2023-04-04 08:41:19 +0530

    update icon link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 function NavBar() {
   return (
     <div className='flex items-center space-x-8 pl-4 py-4 bg-gray-300'>
-      <img src={NavIcon} alt="Logo" className='w-[50px]'/>
+      <Link to="/" className='font-bold text-xl text-red-500'><img src={NavIcon} alt="Logo" className='w-[50px]'/></Link>
       <Link to="/" className='font-bold text-xl text-red-500'>Movies</Link>
       <Link to="/favourite" className='font-bold text-xl text-red-500'>Favourites</Link>
     </div>
